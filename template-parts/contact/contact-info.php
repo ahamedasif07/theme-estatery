66bba9f73651bfa330af3ed47ccc3ed873e5e0a9
ahamedasif07 <rxasif31@gmail.com>
2026-04-15 17:05:22 +0600

    gray remove

--- template-parts/contact/contact-info.php
+++ template-parts/contact/contact-info.php
@@ -115,7 +115,7 @@
         </div>
 
         <div
-            class="mt-20 w-full h-[450px] rounded-[2.5rem] overflow-hidden border border-secondary/5 shadow-2xl grayscale transition-all duration-700 hover:grayscale-0">
+            class="mt-20 w-full h-[450px] rounded-[2.5rem] overflow-hidden border border-secondary/5 shadow-2xl  transition-all duration-700 hover:grayscale-0">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3590.963471012879!2d-80.13264982361663!3d25.77174780821953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b48999335805%3A0xc6829148d8881b7a!2s2892%20Ocean%20Dr%2C%20Miami%20Beach%2C%20FL%2033139%2C%20USA!5e0!3m2!1sen!2sbd!4v1713175000000!5m2!1sen!2sbd"
                 width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy">
